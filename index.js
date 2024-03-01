3f9f5a758945bb9d9af7f822c112ce14197d20de
vandercs27 <152231982+vandercs27@users.noreply.github.com>
2024-03-01 09:04:06 -0300

    subi os arquivos do meu site

---
+++ index.js
@@ -0,0 +1,88 @@
+
+const morro = document.querySelector(".morro")
+const arv = document.querySelector(".arvore")
+const trailer = document.querySelector(".trailer")
+const talita = document.querySelector(".talita")
+const bruno = document.querySelector(".bruno")
+const andrey = document.querySelector(".andrey")
+const ravi = document.querySelector(".ravi")
+const vivi = document.querySelector(".vivi")
+const katrina = document.querySelector(".katrina")
+const stanley = document.querySelector(".stanley")
+const chico = document.querySelector(".chico")
+const amanda = document.querySelector(".amanda")
+const aviao = document.querySelector(".aviao")
+const faca = document.querySelector(".faca")
+const boxe = document.querySelector(".boxe")
+
+
+
+
+
+const urlMorro = "morro.html"
+const urlArvore = "arvore.html"
+const urlTrailer = "trailer.html"
+const urlTalita = "talita.html"
+const urlBruno = "bruno.html"
+const urlAndrey = "andrey.html"
+const urlRavi= "ravi.html"
+const urlVivi = "vivi.html"
+const urlKatrina= "katrina.html"
+const urlStanley = "stanley.html"
+const urlChico = "chico.html"
+const urlAmanda = "amanda.html"
+const urlAviao = "aviao.html"
+const urlFaca = "faca.html"
+const urlBoxe = "boxe.html"
+
+
+
+
+morro.addEventListener("click", ()=>{
+    window.open(urlMorro)
+});
+
+arv.addEventListener("click", ()=>{
+    window.open(urlArvore)
+})
+trailer.addEventListener("click", ()=>{
+    window.open(urlTrailer)
+})
+talita.addEventListener("click", ()=>{
+    window.open(urlTalita)
+    
+})
+bruno.addEventListener("click", ()=>{
+    window.open(urlBruno)
+})
+andrey.addEventListener("click", ()=>{
+    window.open(urlAndrey)
+})
+ravi.addEventListener("click", ()=>{
+    window.open(urlRavi)
+})
+vivi.addEventListener("click", ()=>{
+    window.open(urlVivi)
+})
+katrina.addEventListener("click", ()=>{
+    window.open(urlKatrina)
+})
+stanley.addEventListener("click", ()=>{
+    window.open(urlStanley)
+})
+chico.addEventListener("click", ()=>{
+    window.open(urlChico)
+})
+amanda.addEventListener("click", ()=>{
+    window.open(urlAmanda)
+})
+aviao.addEventListener("click", ()=>{
+    window.open(urlAviao)
+})
+faca.addEventListener("click", ()=>{
+    window.open(urlFaca)
+})
+boxe.addEventListener("click", ()=>{
+    window.open(urlBoxe)
+})
+
